5812beeef8afa9568b5015037448587fe66f7e05
korangi <korangibipin7@gmail.com>
2021-12-03 10:12:35 +0545

    updating

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,9 @@
-import { Link, graphql, useStaticQuery } from "gatsby"
+import { Link, graphql } from "gatsby"
+//import { useStaticQuery } from "gatsby"
 import React from "react"
 
 export default function Navbar() {
-  const data = useStaticQuery(graphql`
+ /* const data = useStaticQuery(graphql`
     query SiteInfo {
       site {
         siteMetadata {
@@ -11,12 +12,10 @@ export default function Navbar() {
       }
     }
   `)
-
-    const { title } = data.site.siteMetadata
-
+  const { title } = data.site.siteMetadata */
   return (
     <nav>
-      <h1>{ title }</h1>
+      <h1>Portfolio</h1>
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
