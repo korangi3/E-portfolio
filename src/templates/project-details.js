7ff3811b192be2536e54eba81d0669324fd1fa20
korangi <korangibipin7@gmail.com>
2021-12-04 15:13:57 +0545

    updating

---
+++ src/templates/project-details.js
@@ -0,0 +1,33 @@
+import { graphql } from 'gatsby'
+import React from 'react'
+import Layout from '../components/Layout'
+import * as styles from '../styles/projects-details.module.css'
+
+export default function ProjectDetails({data}) {
+
+    const { html } = data.markdownRemark
+    const { title, stack } = data.markdownRemark.frontmatter
+    return (
+        <Layout>
+            <div className={styles.details}>
+                <h2>{ title }</h2>
+                <h3>{ stack }</h3>
+                <div className={styles.html} dangerouslySetInnerHTML={{ __html: html }} />
+            </div>
+        </Layout>
+    )
+}
+
+export const query = graphql`
+query ProjectDetails($slug: String) {
+    markdownRemark(frontmatter: {slug: {eq: $slug}}) {
+      frontmatter {
+        stack
+        title
+      }
+      html
+    }
+  }
+  
+`
+  
